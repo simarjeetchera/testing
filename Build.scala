340f348c481c66cc7037039d7d7e04eff451aa45
SIMARJEET SINGH CHERA <test.simarjeet.chera>
2013-12-28 15:02:43 +0530

    init

---
+++ Build.scala
@@ -0,0 +1,31 @@
+import sbt._
+import Keys._
+
+object MinimalBuild extends Build {
+  
+
+  lazy val buildVersion = "2.2.0"
+  lazy val playVersion = "2.2.0"
+  
+  lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
+  lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+  lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe
+  
+  lazy val root = Project(id = "play-plugins-mailer", base = file("."), settings = Project.defaultSettings).settings(
+    version := buildVersion,
+    scalaVersion := "2.10.2",
+    publishTo <<= (version) { version: String =>
+                val nexus = "https://private-repo.typesafe.com/typesafe/"
+                if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "maven-snapshots/")
+                else Some("releases" at nexus + "maven-releases/")
+    },
+    organization := "com.typesafe",
+    resolvers += repo,
+    javacOptions ++= Seq("-source","1.6","-target","1.6", "-encoding", "UTF-8"),
+    javacOptions += "-Xlint:unchecked",
+    libraryDependencies += "com.typesafe.play" %% "play" % playVersion % "provided",
+    libraryDependencies += "org.apache.commons" % "commons-email" % "1.2",
+    libraryDependencies += "com.typesafe" %% "play-plugins-util" % buildVersion ,
+    libraryDependencies	+= "com.typesafe" %% "play-plugins-mailer" % "2.1.0" 	
+  )
+}
---
+++ plugins.sbt.bak
@@ -0,0 +1,10 @@
+// Comment to get more information during initialization
+logLevel := Level.Warn
+
+// The Typesafe repository
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+// Use the Play sbt plugin for Play projects
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % System.getProperty("play.version"))
+
+//addSbtPlugin("com.typesafe" % "play-plugins-mailer_2.9.1" % "2.0.4")
